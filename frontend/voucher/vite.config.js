72a07c09f8c46073c9af1a1802729ce86bf3b18e
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2026-06-30 05:54:10 +0300

    fix deploy

--- frontend/voucher/vite.config.js
+++ frontend/voucher/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/voucher/',
   server: {
     host: true,
     allowedHosts: ['sternitic-inocencia-erringly.ngrok-free.dev']
